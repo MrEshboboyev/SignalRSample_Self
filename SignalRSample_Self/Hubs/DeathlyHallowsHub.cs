d88075ee2d2bb2c95f406291cdb7faf6ccf91bbf
mreshboboyev <mreshboboyev@gmail.com>
2023-12-29 23:09:06 +0500

    DeathlyHallowsHub.cs created Hubs folder and GetRaceStatus() method writed (return dictionary)

---
+++ SignalRSample_Self/Hubs/DeathlyHallowsHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample_Self.Hubs
+{
+    public class DeathlyHallowsHub : Hub
+    {
+        public Dictionary<string, int> GetRaceStatus()
+        {
+            return SD.DealthyHallowRace;
+        }
+    }
+}
