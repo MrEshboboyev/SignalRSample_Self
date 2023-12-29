c11884174c81a3b4b74a7ab9ffc6e13fc7e8597d
mreshboboyev <mreshboboyev@gmail.com>
2023-12-29 21:44:18 +0500

    usersCount.js for Client-Side Connection status

---
+++ SignalRSample_Self/wwwroot/js/usersCount.js
@@ -0,0 +1,27 @@
+﻿// create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hub/userCount").build();
+
+
+// connect to methods that hub invokes aka receives notifications from Hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+// invoke hub methods aka send information to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+// start connection
+function fulfilled() {
+    // do something to start
+    console.log("Connection to User Hub successfull!");
+}
+
+function rejected() {
+    // rejected logs
+    console.log("Connection to User Hub failed!");
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
